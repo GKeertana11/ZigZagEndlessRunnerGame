66ba6307ea623aa2a9ec6925bd941e8db0e618dd
KeertanaG <keertana.govte@yesgnome.com>
2022-03-21 18:28:15 +0530

    Random coins are spawned. Score updated.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,43 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     public float speed;
-   public Vector3 direction;
+    public Vector3 direction;
+    public int score;
+    public Text ScoreText;
+    
+    // public TileSpawnManager TileSpawn;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //TileSpawn =GameObject.Find("TileSpawnManager").GetComponent<TileSpawnManager>();
+       
     }
+
 
     // Update is called once per frame
     void Update()//if player moving in forward make him move left, if not make him move forward    
-    { 
-        if(Input.GetMouseButtonDown(0))
+    {
+        if (Input.GetMouseButtonDown(0))
 
         {
-            if(Vector3.forward==direction)
+            if (Vector3.forward == direction)
             {
-                direction=Vector3.left;
+                direction = Vector3.left;
             }
             else
             {
                 direction = Vector3.forward;
             }
 
-            
+
         }
         transform.Translate(direction * speed * Time.deltaTime);
 
     }
 
+
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        // if (gameObject.tag=="")
+
+        if (other.gameObject.tag == "Tile")
+        {
+            // TileSpawn.SpawnTile();
+            TileSpawnManager.Instance.SpawnTile();
+        }
+
+    }
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.tag=="Tile")
+        if (collision.gameObject.tag == "coin")
         {
-            Destroy(collision.gameObject,5.0f);
+            collision.gameObject.SetActive(false);
+            score = score + 1;
+            Debug.Log(score);
+            ScoreText.text = score.ToString();
         }
     }
+
+    /*RayCast: 
+
+  /*  private void OnCollisionExit(Collision collision) 
+    {
+        
+    }*/
+
+
+
 }
+
--- Assets/Scripts/TileSpawnManager.cs
+++ Assets/Scripts/TileSpawnManager.cs
@@ -7,15 +7,41 @@ public class TileSpawnManager : MonoBehaviour
     public GameObject currentTile;
    GameObject RightTile;
     public GameObject[] TilesPrefab;
+    public float time;
 
     // Start is called before the first frame update
+
+    //SingleTon Pattern: Can only create single object out of it. We can't create multiple objects from it.
+    private static TileSpawnManager instance;
+
+
+    //static TileSpawnManager Instance { get => instance; }
+   /* private void Awake()
+    {
+        if(instance==null)
+        {
+            instance = this;
+        }
+      
+    }*/
+   public static TileSpawnManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = GameObject.FindObjectOfType<TileSpawnManager>();
+            }
+            return instance;
+        }
+    }
+
     void Start()
     {
-
         for (int i = 0; i < 10; i++)
         {
-            int k = Random.Range(0, 2);
-            currentTile = Instantiate(TilesPrefab[k], currentTile.transform.GetChild(k).position, Quaternion.identity);
+
+            SpawnTile();
         }
 
     }
@@ -24,8 +50,29 @@ public class TileSpawnManager : MonoBehaviour
     void Update()
     {
 
-   
+       /* time = time + Time.deltaTime;
+        if(time>6f)
+        {
+            Destroy(GameObject.FindGameObjectWithTag("Tile"));
+            time = 0;
+        }*/
+    
         
         
     }
+     
+    public void SpawnTile()
+    {
+        int index = Random.Range(0, 10);
+        if (index == 2)
+        {
+            currentTile.transform.GetChild(3).gameObject.SetActive(true);
+        }
+            int k = Random.Range(0, 2);
+            currentTile = Instantiate(TilesPrefab[k], currentTile.transform.GetChild(k).position, Quaternion.identity);
+
+        
+    }
+
+  
 }
